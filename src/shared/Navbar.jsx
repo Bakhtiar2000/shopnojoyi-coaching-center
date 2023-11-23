f9bd69abf0d4c4fbbb390a07b76d9b9a06545a49
Bakhtiar2000 <bakhtiarfahim360@gmail.com>
2023-11-23 18:24:25 +0600

    nabar update

--- src/shared/Navbar.jsx
+++ src/shared/Navbar.jsx
@@ -7,7 +7,6 @@ const Navbar = () => {
     const [isMenuOpen, setIsMenuOpen]= useState(false)
     const [navState, setNavState] = useState(false);
 
-
     const onNavScroll = () => {
         if (window.scrollY > 100) {
             setNavState(true);
@@ -44,20 +43,20 @@ const Navbar = () => {
                 {/* Toggle Icon */}
                <div className='lg:hidden inline-block'>
                     <label className="btn btn-circle swap swap-rotate bg-transparent hover:text-black">
-                            <input onClick={() => setIsMenuOpen(isMenuOpen => !isMenuOpen)} type="checkbox" />  
+                        <input onClick={() => setIsMenuOpen(isMenuOpen => !isMenuOpen)} type="checkbox" checked={isMenuOpen} />  
 
-                            {/* hamburger icon */}
-                            <svg className="swap-off fill-current" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 512 512"><path d="M64,384H448V341.33H64Zm0-106.67H448V234.67H64ZM64,128v42.67H448V128Z"/></svg>
+                        {/* hamburger icon */}
+                        <svg className="swap-off fill-current" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 512 512"><path d="M64,384H448V341.33H64Zm0-106.67H448V234.67H64ZM64,128v42.67H448V128Z"/></svg>
 
-                            {/* close icon */}
-                            <svg className="swap-on fill-current" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 512 512"><polygon points="400 145.49 366.51 112 256 222.51 145.49 112 112 145.49 222.51 256 112 366.51 145.49 400 256 289.49 366.51 400 400 366.51 289.49 256 400 145.49"/></svg>
-                        </label>
+                        {/* close icon */}
+                        <svg className="swap-on fill-current" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 512 512"><polygon points="400 145.49 366.51 112 256 222.51 145.49 112 112 145.49 222.51 256 112 366.51 145.49 400 256 289.49 366.51 400 400 366.51 289.49 256 400 145.49"/></svg>
+                    </label>
                </div>
 
                 {
                     isMenuOpen &&
                     // NavItems
-                    <ul className="absolute lg:hidden top-24 right-2 duration-300 border border-primary bg-white z-50 p-2 menu w-56 rounded-box">
+                    <ul onClick={() => setIsMenuOpen(false)} className="absolute lg:hidden top-24 right-2 duration-300 border border-primary bg-white z-50 p-2 menu w-56 rounded-box">
                         <NavItems />
                     </ul>
                 }
